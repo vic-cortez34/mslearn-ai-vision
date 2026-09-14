c88241fe24b3f2fd310d7f23d8c95c254b0f6c73
Victor Cortez <victor.crtz36@gmail.com>
2026-09-14 17:30:02 -0500

    Integrate OpenAI client for image generation and add image saving functionality

--- Labfiles/image-client/python/image-client.py
+++ Labfiles/image-client/python/image-client.py
@@ -2,7 +2,11 @@ import os
 import json
 
 # Add references
-
+from dotenv import load_dotenv
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from openai import OpenAI
+import requests
+import base64
 
 def main(): 
 
@@ -17,7 +21,20 @@ def main():
         model_deployment =  os.getenv("MODEL_DEPLOYMENT")
         
         # Initialize the client
-        
+        credential = DefaultAzureCredential(
+            exclude_environment_credential=True,
+            exclude_managed_identity_credential=True
+        )
+        token_provider = get_bearer_token_provider(
+            credential,
+            "https://cognitiveservices.azure.com/.default"
+        )
+
+        client = OpenAI(
+            base_url=endpoint,
+            api_key=token_provider(),
+        )
+
         
 
          
@@ -33,7 +50,15 @@ def main():
                 continue
             
             # Generate an image
-            
+            img = client.images.generate(
+                model=model_deployment,
+                prompt=input_text,
+                n=1
+            )
+
+            json_response = json.loads(img.model_dump_json())
+            image_data = json_response["data"][0].get("b64_json")
+            image_data_in_bytes = base64.b64decode(image_data)
 
             # save the image
             img_no += 1
