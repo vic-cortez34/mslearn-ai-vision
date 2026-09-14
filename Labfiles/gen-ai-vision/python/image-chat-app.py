8e5389807715bdf23d9e2a6de0dd77b292956649
Victor Cortez <victor.crtz36@gmail.com>
2026-09-14 16:30:53 -0500

    First commit

--- Labfiles/gen-ai-vision/python/image-chat-app.py
+++ Labfiles/gen-ai-vision/python/image-chat-app.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Add references
-
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 def main(): 
 
@@ -21,6 +22,12 @@ def main():
 
 
         # Create an OpenAI client
+        credential = DefaultAzureCredential()
+        token_provider = get_bearer_token_provider(credential, "https://ai.azure.com/.default")
+        client = OpenAI(
+             base_url=openai_endpoint,
+             api_key=token_provider()
+        )
         
 
 
@@ -39,7 +46,42 @@ def main():
                 print("Getting a response ...\n")
 
 
-                # Get a response to image input
+                # Get a response to image input (URL)
+                # image_url = "https://microsoftlearning.github.io/mslearn-ai-vision/Labfiles/gen-ai-vision/orange.jpeg"
+                # response = client.responses.create(
+                #      model=model_deployment,
+                #      input=[
+                #         {"role": "developer", "content": system_message},
+                #         { "role": "user", "content": [  
+                #             { "type": "input_text", "text": prompt},
+                #             { "type": "input_image", "image_url": image_url}
+                #         ]} 
+                #     ]
+                # )
+
+
+                # Get a response to image input (Local image)
+                image_path = Path("mystery-fruit.jpeg")
+                image_format = "jpeg"
+                with open(image_path, "rb") as image_file:
+                     image_data = base64.b64encode(image_file.read()).decode("utf-8")
+
+                data_url = f"data:image/{image_format};base64,{image_data}"
+
+                response = client.responses.create(
+                    model=model_deployment,
+                    input=[
+                        {"role": "developer", "content": system_message},
+                        { "role": "user", "content": [  
+                            { "type": "input_text", "text": prompt},
+                            { "type": "input_image", "image_url": data_url}
+                        ]} 
+                    ]
+                )
+
+                print(response.output_text)
+
+                
                     
 
 
